53586f58b39990927c163f5bb56b078c50fe9825
FCroccoli <nandique@gmail.com>
2023-03-09 05:01:59 -0300

    feat: added create method to serializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Comment
-from django.shortcuts import get_object_or_404
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -9,3 +8,14 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ['id', 'comment', 'created_at', 'user', 'publication']
         read_only_fields = ['id', 'created_at', 'user', 'publication']
+
+    def create(self, validated_data):
+        return Comment.objects.create(**validated_data)
+
+    def update(self, instance: Comment, validated_data: dict) -> Comment:
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        instance.save()
+
+        return instance
